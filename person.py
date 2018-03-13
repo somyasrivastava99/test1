e5aa7ffbe85276e9f857b2d001497156bd4cf325
somyasrivastava99 <say.somyasrivastava@gmail.com>
2018-03-13 16:07:12 +0530

    Initial commit
    changes done

---
+++ person.py
@@ -0,0 +1,20 @@
+import random
+
+chars = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,./;'[]!@#$%^&*()_+~\|:"<>?'''
+times = int(input('[.]Number of passwords?\n[.]'))
+length = int(input('[.]Password length?\n[.]'))
+
+f = open('password.txt','w')
+
+for i in range(times):
+	password = ''
+	for j in range(length):
+		password += random.choice(chars)
+
+	print('[.]',password,sep='')
+	
+	
+	password = password + '\n'
+	f.write(password)
+
+f.close()
