d9738387175ebf5fe32dc6dc0c5cc2996cfa0534
somyasrivastava99 <say.somyasrivastava@gmail.com>
2018-03-13 16:23:51 +0530

    Update

--- person.py
+++ person.py
@@ -4,7 +4,7 @@ chars = '''ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789,./;'[]
 times = int(input('[.]Number of passwords?\n[.]'))
 length = int(input('[.]Password length?\n[.]'))
 
-f = open('password.txt','w')
+f = open('C:\Users\DELL\Documents\GitHub\test1','w')
 
 for i in range(times):
 	password = ''
